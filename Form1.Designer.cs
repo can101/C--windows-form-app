943ecc6ee3389eeb4f1fd3fe4dfd83661e472c49
can101 <can896373@gmail.com>
2021-11-01 16:02:41 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "yazı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(352, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "yapılacak işlem";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 113);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "sonuç";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 116);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(57, 38);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(183, 39);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(19, 208);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(194, 134);
+            this.listBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 149);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 32);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "işlemi yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 149);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 32);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "işlemi kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 41);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 57);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(151, 57);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(105, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Yazının tamamı büyük harf yazılması.",
+            "Yazının tamamı küçük harf yazılması.",
+            "Yazının içinden bir metin parçacığının ilk karşılaşma yerinin bulunması.",
+            "Yazının içinden bir metin parçacığının son karşılaşma yerinin bulunması.",
+            "Yazının içindeki bir değerin başka bir değer ile değiştirilmesi.",
+            "Metnin belirtilen karakter yerinden başlayarak yazı silmesi.",
+            "Metnin belirtilen karakter yerinden başlayarak metin parçası yazması."});
+            this.comboBox1.Location = new System.Drawing.Point(250, 40);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(310, 21);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(244, 208);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(285, 104);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Paramatreler";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Can_Celik_190111013_Homework_example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            groupBox1.Visible= false;
+            listBox1.Visible=false;
+            label4.Visible=false;
+            button2.Visible=false;
+            button1.Enabled=false;
+            comboBox1.Text = "Bir işlem seçiniz.";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selected = comboBox1.SelectedIndex;
+            string txt = textBox1.Text;
+            switch (selected)
+            {
+                case 0:
+                case 1:
+                    button1.Enabled = true;
+                    groupBox1.Visible = false;
+                    break;
+                case 2:
+                    button1.Enabled = true;
+                    groupBox1.Visible = true;
+                    label5.Text = "aranacak kelime";
+                    label6.Visible = false;
+                    textBox3.Visible = false;
+                    break;
+                case 3:
+                    button1.Enabled = true;
+                    groupBox1.Visible = true;
+                    label5.Text = "aranacak kelime";
+                    label6.Visible = false;
+                    textBox3.Visible = false;
+                    break;
+                case 4:
+                    groupBox1.Visible = true;
+                    label5.Text = "Eski deger";
+                    label6.Text = "Yeni deger";
+                    label6.Visible = true;
+                    textBox2.Visible=true;
+                    textBox3.Visible=true;
+                    button1.Enabled = true;
+                    break;
+                case 5:
+                    groupBox1.Visible = true;
+                    label5.Text = "Baslangic";
+                    label6.Visible = false;
+                    textBox3.Visible = false;
+                    textBox2.Visible = true;
+                    button1.Enabled = true;
+                    break;
+                case 6:
+                    groupBox1.Visible = true;
+                    label5.Text = "Baslangic";
+                    label6.Text = "Adet";
+                    label6.Visible = true;
+                    textBox2.Visible = true;
+                    textBox3.Visible = true;
+                    button1.Enabled = true;
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int selected = comboBox1.SelectedIndex;
+            string txt = textBox1.Text;
+            if(comboBox1.Text != null && comboBox1.Text != "" && textBox1.Text != null && textBox1.Text != "")
+            {
+                switch (selected)
+                {
+                    case 0:
+                        label4.Text = txt.ToUpper();
+                        label4.Visible=true;
+                        button2.Visible = true;
+                        button2.Enabled=true;
+                        break;
+                    case 1:
+                        label4.Text = txt.ToLower();
+                        label4.Visible = true;
+                        button2.Visible = true;
+                        button2.Enabled = true;
+                        break;
+                    case 2:
+                        if (textBox2.Text.Length != 0 && textBox2.Text != "" && textBox2.Text != null)
+                        {
+                            label4.Visible = true;
+                            label4.Text = txt.IndexOf(textBox2.Text).ToString();
+                            button2.Visible = true;
+                            button2.Enabled = true;
+                            textBox2.Text = "";
+                        }
+                        else
+                        {
+                            MessageBox.Show("Lutfen parametereyi doldurmayi unutmayiniz ! :(");
+                        }
+                        break;
+                    case 3:
+                        if (textBox2.Text.Length != 0 && textBox2.Text != "" && textBox2.Text != null)
+                        {
+                            label4.Visible = true;
+                            label4.Text = txt.LastIndexOf(textBox2.Text).ToString();
+                            button2.Visible = true;
+                            button2.Enabled = true;
+                            textBox2.Text = "";
+                        }
+                        else
+                        {
+                            MessageBox.Show("Lutfen parametereyi doldurmayi unutmayiniz ! :(");
+                        }
+                        break;
+                    case 4:
+                        if(textBox1.Text != null && textBox1.Text != "")
+                        {
+                            if (textBox2.Text != null && textBox2.Text != "" && textBox3.Text != null && textBox3.Text != "")
+                            {
+                                label4.Text = txt.Replace(textBox2.Text, textBox3.Text);
+                                label4.Visible = true;
+                                button2.Visible = true;
+                                button2.Enabled = true;
+                                button1.Enabled = false;
+                                textBox2.Text = "";
+                                textBox3.Text = "";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Lutfen iki parametereyi doldurmayi unutmayiniz ! :(");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Lutfen yazi bolumunude  doldurunuz ! :(");
+                        }
+                        break;
+                    case 5:
+                        
+                        if (textBox2.Text != null && textBox2.Text != "")
+                        {
+                           label4.Visible = true;
+                        label4.Text = txt.Remove(Convert.ToInt32(textBox2.Text));
+                        button2.Visible = true;
+                        button2.Enabled = true;
+                        textBox2.Text = "";
+                        }
+                        else
+                        {
+                            MessageBox.Show("Lutfen parametereyi doldurmayi unutmayiniz ! :(");
+                        }
+                        break;
+                    case 6:
+
+                        if (textBox2.Text != null && textBox2.Text != "" && textBox3.Text != null && textBox3.Text != "")
+                        {
+                            label4.Visible = true;
+                            label4.Text = txt.Substring(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+                            button2.Visible = true;
+                            button2.Enabled = true;
+                            textBox2.Text = "";
+                            textBox3.Text = "";
+                        }
+                        else
+                        {
+                            MessageBox.Show("Lutfen iki parametereyi doldurmayi unutmayiniz ! :(");
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lutfen bir yazi girip ve islem seciniz!");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(label4.Text);
+            textBox1.Text = "";
+            label4.Text = "";
+            comboBox1.Text = "Bir işlem seçiniz.";
+            listBox1.Visible = true;
+            button2.Enabled = false;
+            button1.Enabled=false;
+            groupBox1.Visible = false;
+        }
+    }
+}
